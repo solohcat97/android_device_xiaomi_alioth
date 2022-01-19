4026c5257bdfe9a736249cb51ebb0b46d1fed1ec
Dyneteve <Dyneteve>
2022-01-19 17:49:13 +0545

    alioth:Force enable voLTE\voWifi\viLTE support

--- device.mk
+++ device.mk
@@ -77,6 +77,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Force voLTE/voWIFI/viLTE
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
+
 # Adaptive charging
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/adaptivecharging.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/adaptivecharging.xml
