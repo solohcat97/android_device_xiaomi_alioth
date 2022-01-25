7fde6ded86a69f469d8c0a2398b2c1fb4633472a
Eidoron1 <eidoronp67@gmail.com>
2022-01-25 20:20:13 +0545

    Revert "alioth:Fixed error while building pixel launcher"
    This reverts commit 4c8f0f639f3ba5e0d67e189a5c7f703ad0bce5fc.

--- PixelLauncher/Android.mk
+++ PixelLauncher/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := PixelLauncher 
+LOCAL_MODULE := Pixel Launcher 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := NexusLauncherRelease.apk
 LOCAL_CERTIFICATE := platform
